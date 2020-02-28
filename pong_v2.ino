5ba86d23aae1de4db0c43c7cf17feb683fecf2d7
Nicolas <salocin.sam@gmail.com>
2020-02-28 18:29:54 +0100

    added two screens + take one me music

--- pong_v2.ino
+++ pong_v2.ino
@@ -1,4 +1,26 @@
-#include "LedControl.h"
+#include "LedControl.h"   //include the library ledcontrol from this site : https://github.com/wayoda/LedControl
+
+
+//CREDITS  
+
+
+
+
+/* 
+
+  https://howtomechatronics.com/tutorials/arduino/8x8-led-matrix-max7219-tutorial-scrolling-text-android-control-via-bluetooth/
+  by Dejan Nedelkovski, www.HowToMechatronics.com
+
+  https://labs.bsoares.com/ping-pong-com-matriz-de-leds-8x8-ee13cce39007
+  @author Bruno Soares
+
+  https://randomnerdtutorials.com/guide-for-8x8-dot-matrix-max7219-with-arduino-pong-game/
+  Play pong on an 8x8 matrix - project from itopen.it
+
+  Take on me
+  More songs available at https://github.com/robsoncouto/arduino-songs                                            
+  Robson Couto, 2019
+*/
 
 LedControl lc=LedControl(12,11,10,2);
 unsigned long delaytime=500;
@@ -114,6 +136,140 @@ byte five[]={        // Used to diplay a '5' for when displaying the score
   B00000000,
   B00000000
 };
+#define NOTE_B0  31
+#define NOTE_C1  33
+#define NOTE_CS1 35
+#define NOTE_D1  37
+#define NOTE_DS1 39
+#define NOTE_E1  41
+#define NOTE_F1  44
+#define NOTE_FS1 46
+#define NOTE_G1  49
+#define NOTE_GS1 52
+#define NOTE_A1  55
+#define NOTE_AS1 58
+#define NOTE_B1  62
+#define NOTE_C2  65
+#define NOTE_CS2 69
+#define NOTE_D2  73
+#define NOTE_DS2 78
+#define NOTE_E2  82
+#define NOTE_F2  87
+#define NOTE_FS2 93
+#define NOTE_G2  98
+#define NOTE_GS2 104
+#define NOTE_A2  110
+#define NOTE_AS2 117
+#define NOTE_B2  123
+#define NOTE_C3  131
+#define NOTE_CS3 139
+#define NOTE_D3  147
+#define NOTE_DS3 156
+#define NOTE_E3  165
+#define NOTE_F3  175
+#define NOTE_FS3 185
+#define NOTE_G3  196
+#define NOTE_GS3 208
+#define NOTE_A3  220
+#define NOTE_AS3 233
+#define NOTE_B3  247
+#define NOTE_C4  262
+#define NOTE_CS4 277
+#define NOTE_D4  294
+#define NOTE_DS4 311
+#define NOTE_E4  330
+#define NOTE_F4  349
+#define NOTE_FS4 370
+#define NOTE_G4  392
+#define NOTE_GS4 415
+#define NOTE_A4  440
+#define NOTE_AS4 466
+#define NOTE_B4  494
+#define NOTE_C5  523
+#define NOTE_CS5 554
+#define NOTE_D5  587
+#define NOTE_DS5 622
+#define NOTE_E5  659
+#define NOTE_F5  698
+#define NOTE_FS5 740
+#define NOTE_G5  784
+#define NOTE_GS5 831
+#define NOTE_A5  880
+#define NOTE_AS5 932
+#define NOTE_B5  988
+#define NOTE_C6  1047
+#define NOTE_CS6 1109
+#define NOTE_D6  1175
+#define NOTE_DS6 1245
+#define NOTE_E6  1319
+#define NOTE_F6  1397
+#define NOTE_FS6 1480
+#define NOTE_G6  1568
+#define NOTE_GS6 1661
+#define NOTE_A6  1760
+#define NOTE_AS6 1865
+#define NOTE_B6  1976
+#define NOTE_C7  2093
+#define NOTE_CS7 2217
+#define NOTE_D7  2349
+#define NOTE_DS7 2489
+#define NOTE_E7  2637
+#define NOTE_F7  2794
+#define NOTE_FS7 2960
+#define NOTE_G7  3136
+#define NOTE_GS7 3322
+#define NOTE_A7  3520
+#define NOTE_AS7 3729
+#define NOTE_B7  3951
+#define NOTE_C8  4186
+#define NOTE_CS8 4435
+#define NOTE_D8  4699
+#define NOTE_DS8 4978
+#define REST      0
+
+
+// change this to make the song slower or faster
+int tempo = 140;
+
+// change this to whichever pin you want to use
+int buzzer = 2;
+
+// notes of the moledy followed by the duration.
+// a 4 means a quarter note, 8 an eighteenth , 16 sixteenth, so on
+// !!negative numbers are used to represent dotted notes,
+// so -4 means a dotted quarter note, that is, a quarter plus an eighteenth!!
+int melody[] = {
+
+  // Take on me, by A-ha
+  // Score available at https://musescore.com/user/190926/scores/181370
+  // Arranged by Edward Truong
+
+  NOTE_FS5,8, NOTE_FS5,8,NOTE_D5,8, NOTE_B4,8, REST,8, NOTE_B4,8, REST,8, NOTE_E5,8, 
+  REST,8, NOTE_E5,8, REST,8, NOTE_E5,8, NOTE_GS5,8, NOTE_GS5,8, NOTE_A5,8, NOTE_B5,8,
+  NOTE_A5,8, NOTE_A5,8, NOTE_A5,8, NOTE_E5,8, REST,8, NOTE_D5,8, REST,8, NOTE_FS5,8, 
+  REST,8, NOTE_FS5,8, REST,8, NOTE_FS5,8, NOTE_E5,8, NOTE_E5,8, NOTE_FS5,8, NOTE_E5,8,
+  NOTE_FS5,8, NOTE_FS5,8,NOTE_D5,8, NOTE_B4,8, REST,8, NOTE_B4,8, REST,8, NOTE_E5,8, 
+  
+  REST,8, NOTE_E5,8, REST,8, NOTE_E5,8, NOTE_GS5,8, NOTE_GS5,8, NOTE_A5,8, NOTE_B5,8,
+  NOTE_A5,8, NOTE_A5,8, NOTE_A5,8, NOTE_E5,8, REST,8, NOTE_D5,8, REST,8, NOTE_FS5,8, 
+  REST,8, NOTE_FS5,8, REST,8, NOTE_FS5,8, NOTE_E5,8, NOTE_E5,8, NOTE_FS5,8, NOTE_E5,8,
+  NOTE_FS5,8, NOTE_FS5,8,NOTE_D5,8, NOTE_B4,8, REST,8, NOTE_B4,8, REST,8, NOTE_E5,8, 
+  REST,8, NOTE_E5,8, REST,8, NOTE_E5,8, NOTE_GS5,8, NOTE_GS5,8, NOTE_A5,8, NOTE_B5,8,
+  
+  NOTE_A5,8, NOTE_A5,8, NOTE_A5,8, NOTE_E5,8, REST,8, NOTE_D5,8, REST,8, NOTE_FS5,8, 
+  REST,8, NOTE_FS5,8, REST,8, NOTE_FS5,8, NOTE_E5,8, NOTE_E5,8, NOTE_FS5,8, NOTE_E5,8,
+  
+};
+
+// sizeof gives the number of bytes, each int value is composed of two bytes (16 bits)
+// there are two values per note (pitch and duration), so for each note there are four bytes
+int notes = sizeof(melody) / sizeof(melody[0]) / 2;
+
+// this calculates the duration of a whole note in ms
+int wholenote = (60000 * 4) / tempo;
+
+int divider = 0, noteDuration = 0;
+
 void setSprite(byte *sprite){     //used todisplay all the rows correctly when calling score
     
     for(int r = 0; r < 16; r++){
@@ -353,15 +509,15 @@ void wallCollisons(){
         screen = 0;
         setSprite(smile01);
         screen = 1;
-        setSprite(smile02);
+        setSprite(smile03);
       }
       else if(scoreright==5){
         screen = 1;
         setSprite(smile01);
         screen = 0;
-        setSprite(smile02);
+        setSprite(smile03);
       }
-      
+      takeOnMe();
       delay(2000);
       lc.clearDisplay(0);
       lc.clearDisplay(1);
@@ -379,7 +535,32 @@ void wallCollisons(){
   }  
   
 }
+void takeOnMe(){
+ // iterate over the notes of the melody.
+  // Remember, the array is twice the number of notes (notes + durations)
+  for (int thisNote = 0; thisNote < notes * 2; thisNote = thisNote + 2) {
 
+    // calculates the duration of each note
+    divider = melody[thisNote + 1];
+    if (divider > 0) {
+      // regular note, just proceed
+      noteDuration = (wholenote) / divider;
+    } else if (divider < 0) {
+      // dotted notes are represented with negative durations!!
+      noteDuration = (wholenote) / abs(divider);
+      noteDuration *= 1.5; // increases the duration in half for dotted notes
+    }
+
+    // we only play the note for 90% of the duration, leaving 10% as a pause
+    tone(buzzer, melody[thisNote], noteDuration * 0.9);
+
+    // Wait for the specief duration before playing the next note.
+    delay(noteDuration);
+
+    // stop the waveform generation before the next note.
+    noTone(buzzer);
+  }
+}
 
 void Rackleft(){    //moving the left racket with a joystick
   data = analogRead(leftJoy);
@@ -480,14 +661,14 @@ void racketCollisions(){
   }
   else if (xball==14){   //collsion for rightracket
     if (yball == rightracket+2 && angle == 315 || yball == rightracket+2 && angle == -45 ){    //top left corner
-      angle = 225;
+      angle = 135;
     }
     else if (yball == rightracket+1 ){   //top right
       if ( angle == 315 || angle == -45){
         angle = 180;
       }
       else{
-        angle = 225;
+        angle = 135;
       }
     }
     else if(yball == rightracket ){   //bottom right
@@ -495,11 +676,11 @@ void racketCollisions(){
         angle = 180;
       }
       else{
-        angle=135;
+        angle=-135;
       }
     }
     else if (yball == rightracket-1 && angle == 45 || yball == rightracket-1 && angle == -315){
-      angle = 135;    //bottom corner
+      angle = 225;    //bottom corner
     }
   }
   
@@ -527,3 +708,4 @@ void loop() {
   Rackleft();
   Rackright();
 }
+
